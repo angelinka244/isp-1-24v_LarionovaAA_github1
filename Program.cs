aa82a912611b015f9e00b8f3669b45c060f557e9
Лар кар мар <example@example.com>
2026-02-09 11:20:05 +0500

    lj,

--- Program.cs
+++ Program.cs
@@ -7,4 +7,28 @@
             Console.WriteLine("Hello, World!");
         }
     }
+    public static class Calculator
+    {
+        // Сложение двух чисел
+        public static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+        // Вычитание двух чисел
+        public static double Substract(double a, double b)
+        {
+            return a - b;
+        }
+        // Умножение двух чисел
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        // Деление двух чисел
+        public static double Divide(double a, double b)
+        {
+            return a / b;
+        }
+    }
+
 }
